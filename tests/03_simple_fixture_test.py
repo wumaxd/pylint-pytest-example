45a19887bf5b0e11f42780bf5f10af1dd20847cc
Max <maximilian.wurzer@ibm.com>
2020-06-22 23:01:55 +0200

    Remove global fixture test

--- tests/03_simple_fixture_test.py
+++ tests/03_simple_fixture_test.py
@@ -16,10 +16,3 @@ def local_fixture():
     Fixtures are callables decorated with @fixture
     """
     print("\n(Doing Local Fixture setup stuff!)")
-
-
-def test_with_global_fixture(global_fixture):
-    """
-    Fixtures can also be shared across test files (see conftest.py)
-    """
-    print("Running test_with_global_fixture...")
